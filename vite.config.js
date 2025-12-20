4b79c146e9bf90ef105696a28a45916676b2bc55
glenn <glenniserr0r@gmail.com>
2025-12-21 01:27:34 +0800

    resources/css/app.css

--- vite.config.js
+++ vite.config.js
@@ -8,26 +8,24 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+
                 'resources/css/homepage.css',
                 'resources/js/homepage.js',
-                /********************student login ***********************/
-                'resources/js/student_login.css',
-                'resources/js/student_login.js',
-                /********************student login ***********************/
-                'resources/js/teacher_login.css',
-                'resources/js/teacher_login.js',
-                /********************student login ***********************/
-                'resources/js/admin_login.css',
-                'resources/js/admin_login.js',
-    
+
+                // Student
+                'resources/css/login/student_login.css',
+                'resources/js/login/student_login.js',
+
+                // Teacher
+                'resources/css/login/teacher_login.css',
+                'resources/js/login/teacher_login.js',
+
+                // Admin
+                'resources/css/login/admin_login.css',
+                'resources/js/login/admin_login.js',
             ],
             refresh: true,
         }),
         tailwindcss(),
     ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
 });
