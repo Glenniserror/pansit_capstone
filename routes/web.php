304754df8e57229264372589205b68088b3dedf6
glenn <glenniserr0r@gmail.com>
2025-12-20 23:51:59 +0800

    app/Http/Controllers/Auth/StudentAuthController.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\StudentAuthController;
 
 Route::get('/', function () {
     return view('glenn.homepage');
 });
 
-Route::get('/login', function () {
-    return view('login.student_login');
-});
+// Student login page
+Route::get('/login', [StudentAuthController::class, 'showLoginForm'])->name('student.login');
+
+// Handle login POST
+Route::post('/login', [StudentAuthController::class, 'login'])->name('student.login.submit');
+
+// Handle signup POST
+Route::post('/register', [StudentAuthController::class, 'register'])->name('student.register');
