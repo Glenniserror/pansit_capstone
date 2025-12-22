332d7188d655340dfb1039b975feb63cfe87e4cb
glenn <glenniserr0r@gmail.com>
2025-12-22 20:34:47 +0800

    routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\StudentAuthController;
 use App\Http\Controllers\Auth\TeacherAuthController;
