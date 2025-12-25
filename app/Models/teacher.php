a57b174d0e66ce32e6b10c1962ea4d3b48bcad4b
glenn <glenniserr0r@gmail.com>
2025-12-25 21:07:36 +0800

    app/Models/admin.php

---
+++ app/Models/teacher.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable; // Importante ito
+use Illuminate\Notifications\Notifiable;
+
+class Teacher extends Authenticatable
+{
+    use Notifiable;
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
