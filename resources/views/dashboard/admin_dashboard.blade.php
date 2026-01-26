40bb47cbcfc70b45ff0931cbdd304c7fab8a81c0
glenn <glenniserr0r@gmail.com>
2026-01-26 22:41:11 +0800

    resources/views/dashboard/admin_dashboard.blade.php

--- resources/views/dashboard/admin_dashboard.blade.php
+++ resources/views/dashboard/admin_dashboard.blade.php
@@ -18,8 +18,7 @@
             <div class="logo-section">
                 <div class="logo-icon"> 
                     <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
-                        <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path>
+                        <img src="{{ asset('image/learning.png') }}" alt="admin Icon">
                     </svg>
                 </div>
                 <span class="brand-name">Math Learning Assistant</span>
--- resources/views/dashboard/student_dashboard.blade.php
+++ resources/views/dashboard/student_dashboard.blade.php
@@ -19,8 +19,7 @@
             <div class="logo-section">
                 <div class="logo-icon">
                     <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
-                        <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path>
+                        
                     </svg>
                 </div>
                 <span class="brand-name">Math Learning Assistant</span>
