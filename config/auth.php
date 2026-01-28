517633c12a2ab5e5787b9b10cc6ce102b1bb3522
glenn <glenniserr0r@gmail.com>
2026-01-28 12:01:39 +0800

    config/auth.php

--- config/auth.php
+++ config/auth.php
@@ -1,15 +1,47 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "user_db";
+return [
+// config/auth.php
 
-$conn = new mysqli($host, $user, $password, $database);
+'guards' => [
+    'web' => [
+        'driver' => 'session',
+        'provider' => 'users',
+    ],
 
-if ($conn->connect_error){
-    die("Connection failed: ". $conn->connect_error);
-}
+    // Idagdag itong tatlo:
+    'admin' => [
+        'driver' => 'session',
+        'provider' => 'admins',
+    ],
+    'student' => [
+        'driver' => 'session',
+        'provider' => 'students',
+    ],
+    'teacher' => [
+        'driver' => 'session',
+        'provider' => 'teachers',
+    ],
+],
 
+'providers' => [
+    'users' => [
+        'driver' => 'eloquent',
+        'model' => App\Models\User::class,
+    ],
 
-?>
+    // Idagdag itong tatlo at siguraduhin na tama ang path ng Models:
+    'admins' => [
+        'driver' => 'eloquent',
+        'model' => App\Models\Admin::class,
+    ],
+    'students' => [
+        'driver' => 'eloquent',
+        'model' => App\Models\Student::class,
+    ],
+    'teachers' => [
+        'driver' => 'eloquent',
+        'model' => App\Models\Teacher::class,
+    ],
+],
+];
